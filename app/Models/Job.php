2f25828c4f0ed39498f79137accab07a497c78a1
az-m <alisonzmills@gmail.com>
2025-11-04 11:08:22 +0000

    eloquent chapter

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -2,36 +2,21 @@
 
 namespace App\Models;
 
-use Illuminate\Support\Arr;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-class Job {
-    public static function all(): array {
-        return [
-            [
-                'id' => 1,
-                'title' => 'Director',
-                'salary' => '£80,000'
-            ],
-            [
-                'id' => 2,
-                'title' => 'Developer',
-                'salary' => '£30,000'
-            ],
-            [
-                'id' => 3,
-                'title' => 'Teacher',
-                'salary' => '£38,000'
-            ]
-        ];
+
+class Job extends Model {
+    use HasFactory;
+
+    protected $table = 'job_listings';
+    protected $fillable = ['title', 'salary'];
+
+    public function employer() {
+        return $this->belongsTo(Employer::class);
     }
 
-    public static function find(int $id): array {
-        $job =  Arr::first(static::all(), fn($job) => $job['id'] == $id);
-
-        if (! $job) {
-            abort(404);
-        }
-
-        return $job;
+    public function tags() {
+        return $this->belongsToMany(Tag::class, foreignPivotKey: "job_listing_id");
     }
 }
--- resources/views/jobs.blade.php
+++ resources/views/jobs.blade.php
@@ -3,13 +3,17 @@
         Job Listings
     </x-slot>
 
-    <ul>
-    @foreach ($jobs as $job)
-        <li class="my-1 hover:underline">
-            <a href="/jobs/{{ $job['id'] }}">
-                <strong>{{ $job['title'] }}:</strong> Pays {{ $job['salary'] }} per year.
+    <div class="space-y-2 py-2">
+        @foreach ($jobs as $job)
+            <a href="/jobs/{{ $job['id'] }}" class="block px-4 py-6 border border-zinc-600 rounded-sm shadow-sm shadow-black">
+                <div class="text-lg text-indigo-500">{{ $job->employer->name }}</div>
+                <div>
+                    <strong>{{ $job['title'] }}:</strong> Pays £{{ $job['salary'] }} per year.
+                </div>
             </a>
-        </li>    
-    @endforeach
-    </ul>
+        @endforeach
+        <div>
+            {{ $jobs->links() }}
+        </div>
+    </div>
 </x-layout>
